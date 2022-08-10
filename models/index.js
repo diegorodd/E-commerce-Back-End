b08e24622a97ae3099b48a049d9677238bfa8851
diegorodd <dieegoa@icloud.com>
2022-08-09 23:34:27 -0400

    started and almost finishing the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
+
 
 // Categories have many Products
 
+Category.hasMany(Product);
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+    through: ProductTag
+  });   ProductTag.belongsTo(Tag);
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+    through: ProductTag
+  });
+  ProductTag.belongsTo(Product);
+
 
 module.exports = {
   Product,
